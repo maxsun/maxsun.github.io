970b16aeca5da0151e1990412893ef59c0bbb60b
Max Sun <maxsun@Hawk.local>
2016-03-23 15:31:30 -0700

    Styling updates

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 window.onload = function(){
 
   var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
-  var radius = canvas.width/120;
-  var mousecoords = {x:0, y:0};
-  var fps = 50;
-  var speed = .02;
 
   canvas.width  = window.innerWidth;
   canvas.height = window.innerHeight;
+  
+  var context = canvas.getContext("2d");
+  var radius = canvas.width/280;
+  var mousecoords = {x:0, y:0};
+  var fps = 50;
+  var speed = .02;
 
   function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
@@ -68,31 +69,56 @@ window.onload = function(){
   }
 
   function draw(){
-    context.fillStyle = "#59AFDE"
+    context.fillStyle = "#8BCDFC"
     context.fillRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = "#A2D0E8";
-    for(var i = 0; i < units.length; i++){
-      
-      context.beginPath();
-      context.arc(units[i].x, units[i].y, radius, 0, 2 * Math.PI, false);
-      context.fill();
-      context.stroke();
+    context.fillStyle = "rgb(104, 134, 217)";
 
-      context.stroke();
-      var close = getClose(units[i].x, units[i].y, 100);
-      for(var j = 0; j < close.length; j++){
-        context.beginPath();
-        context.moveTo(units[i].x,units[i].y);
-        context.lineTo(close[j].x, close[j].y);
-        context.strokeStyle = "#A2D0E8";
-        context.lineWidth = .5;
-        context.stroke();
+
+    for(var i = 0; i < units.length; i++){
+      var maxdist = 100;
+      var x = units[i].x;
+      var y = units[i].y;
+
+      for(var j =0; j < units.length; j++){
+        var dist = Math.sqrt(Math.pow(units[j].x - x,2)+Math.pow(units[j].y-y,2));
+        if(dist < maxdist){
+           context.beginPath();
+           context.moveTo(units[i].x,units[i].y);
+           context.lineTo(units[j].x, units[j].y);
+           opacity = dist / maxdist;
+           context.strokeStyle = "rgb("+Math.max(120, Math.floor(opacity*139))+","+Math.max(170, Math.floor(opacity*209))+",252)";
+           context.lineWidth = 1.5;
+           context.stroke();
+        }
+
+        units[i].x += units[i].xv;
+        units[i].y += units[i].yv;
+        if(units[i].x >= canvas.width){
+          units[i].xv *= -1;
+        }
+        if(units[i].x < 0){
+          units[i].xv *= -1;
+        }
+        if(units[i].y >= canvas.height){
+          units[i].yv *= -1;
+        }
+        if(units[i].y < 0){
+          units[i].yv *= -1;
+        }
       }
-      
-    }
-    setTimeout(function() {
-        requestAnimationFrame(draw);
-    }, 1000 / fps);
+  }
+
+  for(var i = 0; i < units.length; i++){
+    context.beginPath();
+    context.arc(units[i].x, units[i].y, radius, 0, 2 * Math.PI, false);
+    context.fill();
+    context.strokeStyle = context.fillStyle;
+    context.stroke();
+  }
+
+  setTimeout(function() {
+      requestAnimationFrame(draw);
+  }, 1000 / fps);
   }
   window.addEventListener("resize", function(){
     canvas.width  = window.innerWidth;
